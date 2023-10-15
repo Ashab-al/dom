feee339b0c5474c151daacb7c2cbbf27782f6b86
Ashab <77648621+Ashab-al@users.noreply.github.com>
2023-10-15 23:47:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const windowButton = document.querySelector('.btn-trigger');
+
+
+windowButton.addEventListener('click', function flash(){
+    document.querySelector('.decor').classList.add('active');
+
+})
